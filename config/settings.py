dfdd86824325e58420df62d387386847cb3cf1e9
MIRJAHON-JS <121972593+MIRJAHON-JS@users.noreply.github.com>
2023-08-04 22:59:21 +0500

    All

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)va)b8wlnvtak=7=o&uh8#m%tb8l%3z87v6m7@z7)d+*8n2u+d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
